3a9ae5a0ee44fef06624770bd3032c86e4a12715
Rahim Alwer <rahim.alwer@gmail.com>
2024-03-16 15:50:48 +1100

    fix: bundle and minify cdn file

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from "rollup";
 import esbuild from "rollup-plugin-esbuild";
 
 import fs from "node:fs";
+import path from "node:path";
 
 export default defineConfig([
+  // NPM
   {
-    input: ["src/index.ts", "src/icons/lazy.ts", "src/element.ts", "src/cdn.ts"],
+    input: ["src/index.ts", "src/icons/lazy.ts", "src/element.ts"],
     treeshake: true,
     output: {
       format: "esm",
@@ -24,6 +26,37 @@ export default defineConfig([
           fs.writeFileSync("dist/server/element.js", "export class MediaIconElement {}");
         },
       },
+    ],
+  },
+  // CDN
+  {
+    input: { cdn: "src/element.ts" },
+    treeshake: true,
+    output: {
+      format: "esm",
+      dir: "dist",
+    },
+    plugins: [
+      {
+        name: "external",
+        async resolveId(id, importer) {
+          if (id.startsWith(".") && !id.includes("lazy")) {
+            const filePath = (await this.resolve(id, importer, { skipSelf: true }))?.id;
+            if (filePath?.includes("src/icons")) {
+              return {
+                id: `./icons/${path.basename(id.replace(".ts", ".js"))}`,
+                external: true,
+              };
+            }
+          }
+        },
+      },
+      esbuild({
+        target: "esnext",
+        platform: "neutral",
+        minify: true,
+        mangleProps: /^_/,
+      }),
     ],
   },
 ]);
